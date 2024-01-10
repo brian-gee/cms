828ec9595876ba63ac2d1eb74bea279daaf9f402
brian-gee <39035211+brian-gee@users.noreply.github.com>
2024-01-10 07:09:06 -0500

    adding lots of packages and cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
+import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [solidJs(), tailwind()],
 });
